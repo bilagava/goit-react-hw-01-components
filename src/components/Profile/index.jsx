81bdb464069f7251a64ead3bc310d446eebb9a56
Yuriy Brovko <ysb.ptc@gmail.com>
2022-07-02 15:43:23 +0300

    component profile

---
+++ src/components/Profile/index.jsx
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types';
+// import css from '../ProfileNetwork/ProfileNetwork.module.css';
+import user from '../user.json';
+
+
+const Profile = () => {
+  return (
+    <div className="profile">
+      <div className="description">
+        <img src={user.avatar} alt="User avatar" className="avatar" />
+        <p className="name">{user.username}</p>
+        <p className="tag">{user.tag}</p>
+        <p className="location">{user.location}</p>
+      </div>
+
+      <ul className="stats">
+        <li>
+          <span className="label">Followers </span>
+          <span className="quantity">{user.stats.followers}</span>
+        </li>
+        <li>
+          <span className="label">Views </span>
+          <span className="quantity">{user.stats.views}</span>
+        </li>
+        <li>
+          <span className="label">Likes </span>
+          <span className="quantity">{user.stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Profile.prototype = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.object,
+};
+
+export default Profile;
