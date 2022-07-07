141150c439bccb0b1f1a7723c8e4bdcddcd14fcb
Yuriy Brovko <ysb.ptc@gmail.com>
2022-07-08 00:03:51 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import Profile from './Profile';
-// import Statistics from './Statistics';
+import Statistics from './Statistics';
 // import FriendList from './FriendList';
 // import TransactionHistory from './TransactionHistory';
 import user from '../Data/user.json';
-// import data from '../Data/data.json';
+import data from '../Data/data.json';
 // import friends from '../Data/friends.json';
 // import transactions from '../Data/transactions.json';
 
@@ -17,8 +17,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      {/* <Statistics title="Upload stats" statData={data} />
-      <FriendList items={friends} />
+      <Statistics title="Upload stats" statData={data} />
+      {/* <FriendList items={friends} />
       <TransactionHistory items={transactions} /> */}
     </div>
   );
